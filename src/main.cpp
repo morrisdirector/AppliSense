3e48405369532108f7309d75168dd55ee7e7e97c
Patrick Morris <patrick@chirohd.com>
2025-04-12 06:22:43 -0400

    flash test

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,35 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+//  D1 Mini Pin Number Reference:
+//  D0  16
+//  D1  5
+//  D2  4
+//  D3  0
+//  D4  2  (also this is the built in LED)
+//  D5  14
+//  D6  12
+//  D7  13
+//  D8  15
+//  TX  1
+//  RX  3
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+// LED pin definition
+const int LED_PIN = 2;
+
+void setup()
+{
+  // Initialize LED pin as output
+  pinMode(LED_PIN, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+  // Turn LED on
+  digitalWrite(LED_PIN, HIGH);
+  // Wait for 1 second
+  delay(1000);
+  // Turn LED off
+  digitalWrite(LED_PIN, LOW);
+  // Wait for 1 second
+  delay(1000);
 }
